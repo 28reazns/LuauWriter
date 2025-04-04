14811e58bacf348e135e478b6eba4b7fac9964e7
28reazns <itsnickyylol@gmail.com>
2025-04-04 13:06:02 -0400

    hi

--- main.py
+++ main.py
@@ -26,16 +26,17 @@ for module in required_modules:
         missing_modules.append(module)
 
 # notify user of missing modules
-if missing_modules and not getattr(sys,'frozen',False): #skip check if EXE
-    for module in missing_modules:
-        print(f"Module {module} is missing. Installing...")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", module], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+if not getattr(sys,'frozen',False): #skip check if EXE
+    if missing_modules:
+        for module in missing_modules:
+            print(f"Module {module} is missing. Installing...")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", module], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    # Clear the output screen after installation
-    if sys.platform == "win32":  # For Windows
-        os.system("cls")
-    else:  # For macOS/Linux
-        os.system("clear")
+        # Clear the output screen after installation
+        if sys.platform == "win32":  # For Windows
+            os.system("cls")
+        else:  # For macOS/Linux
+            os.system("clear")
 else:
     # Check if the config is already in AppData, otherwise copy it there
     appdata_path = os.path.join(os.getenv('APPDATA'), 'LuauWriter', 'config.json')
